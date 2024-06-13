c354cd49e3186c947919505e54d7ac81da7dfcbf
Bonita McBride <bonitamcbride@gmail.com>
2024-06-12 23:04:15 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'grotesk': ['Space Grotesk', 'sans-serif'],
+      'inter': ['Inter', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
+
